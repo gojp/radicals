366289b268b817dfa27fa66e04ab1a5bcf912649
Shawn Smith <shawnpsmith@gmail.com>
2014-03-01 01:03:58 +0900

    start on radicals to kanji test

--- radicals_test.go
+++ radicals_test.go
@@ -1,12 +1,13 @@
 package radicals
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 var (
-	numRadicals    = 252
-	numKanji       = 6355
-	radkfileParser = RadkfileParser{}
-	kradfileParser = KradfileParser{}
+	numRadicals = 252
+	numKanji    = 6355
 )
 
 func TestRadkfileParser(t *testing.T) {
@@ -17,7 +18,6 @@ func TestRadkfileParser(t *testing.T) {
 	if len(got.Radicals) != numRadicals {
 		t.Fatalf("ParseRadkfile length incorrect: got %d, want %d", len(got.Radicals), numRadicals)
 	}
-	radkfileParser = got
 }
 
 func TestKradfileParser(t *testing.T) {
@@ -28,5 +28,36 @@ func TestKradfileParser(t *testing.T) {
 	if len(got.Kanji) != numKanji {
 		t.Fatalf("ParseKradfile length incorrect: got %d, want %d", len(got.Kanji), numKanji)
 	}
-	kradfileParser = got
+}
+
+var radicalToKanjiTests = []struct {
+	radical string
+	kanji   []string
+}{
+	{"入_2", strings.Split("久込入兩兪叺圦懣杁柩滿疚瞞窩糴裲蹣輛陝魎鳰", "")},
+	{"マ_2", strings.Split("桶擬疑凝柔序痛通樋矛勇湧涌予預踊豫舒俑墅慂懋抒揉矜礙糅蛹蹂踴鞣", "")},
+}
+
+func sliceEqual(s1, s2 []string) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	for i, x := range s1 {
+		if s2[i] != x {
+			return false
+		}
+	}
+	return true
+}
+
+func TestRadicalsToKanji(t *testing.T) {
+	r, err := ParseRadkfile("radkfile.utf8")
+	if err != nil {
+		t.Fatalf("ParseRadkfile: %v", err)
+	}
+	for _, tt := range radicalToKanjiTests {
+		if got := r.Radicals[tt.radical]; !sliceEqual(got, tt.kanji) {
+			t.Errorf("TestRadicalsToKanji: got %v, want %v", got, tt.kanji)
+		}
+	}
 }
