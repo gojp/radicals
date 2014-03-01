741a64d550e6c1565e8f0aca16b247a3d4b033ba
Shawn Smith <shawnpsmith@gmail.com>
2014-03-01 13:33:36 +0900

    add stroke count check to tests

--- radicals_test.go
+++ radicals_test.go
@@ -31,9 +31,9 @@ func TestKradfileParser(t *testing.T) {
 }
 
 var radicalToKanjiTests = []struct {
-	radical string
-	strokes int
-	kanji   []string
+	radical     string
+	strokeCount int
+	kanji       []string
 }{
 	{"入", 2, strings.Split("久込入兩兪叺圦懣杁柩滿疚瞞窩糴裲蹣輛陝魎鳰", "")},
 	{"マ", 2, strings.Split("桶擬疑凝柔序痛通樋矛勇湧涌予預踊豫舒俑墅慂懋抒揉矜礙糅蛹蹂踴鞣", "")},
@@ -59,6 +59,9 @@ func TestRadicalsToKanji(t *testing.T) {
 	for _, tt := range radicalToKanjiTests {
 		if got := r[tt.radical].Kanji; !sliceEqual(got, tt.kanji) {
 			t.Errorf("TestRadicalsToKanji(%s): got %v, want %v", tt.radical, got, tt.kanji)
+		}
+		if got := r[tt.radical].StrokeCount; got != tt.strokeCount {
+			t.Errorf("TestRadicalsToKanji(%s): got %d, want %d", tt.radical, got, tt.strokeCount)
 		}
 	}
 }
